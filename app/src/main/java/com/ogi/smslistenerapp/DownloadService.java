52a2ab20adfb16661b3a6490484d21338047cb11
Ogi G Permana <hunterdumay@gmail.com>
2019-07-24 23:14:47 +0700

    Implementasi Broadcast menggunakan bg service dan async

---
+++ app/src/main/java/com/ogi/smslistenerapp/DownloadService.java
@@ -0,0 +1,28 @@
+package com.ogi.smslistenerapp;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class DownloadService extends IntentService {
+
+    public static final String TAG = "DownloadService";
+
+    public DownloadService() {
+        super("DownloadService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "Download file dijalankan");
+        if (intent != null) {
+            try{
+                Thread.sleep(5000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            Intent notifyFinishIntent = new Intent(MainActivity.ACTION_DOWNLOAD_STATUS);
+            sendBroadcast(notifyFinishIntent);
+        }
+    }
+}
--- app/src/main/java/com/ogi/smslistenerapp/MainActivity.java
+++ app/src/main/java/com/ogi/smslistenerapp/MainActivity.java
@@ -1,16 +1,23 @@
 package com.ogi.smslistenerapp;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String ACTION_DOWNLOAD_STATUS = "download_status";
+    private BroadcastReceiver downloadReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         Button btnPermission = findViewById(R.id.btn_permission);
         btnPermission.setOnClickListener(this);
+        Button btnDownload = findViewById(R.id.btn_download);
+        btnDownload.setOnClickListener(this);
+
+        downloadReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(DownloadService.TAG, "Download selesai");
+                Toast.makeText(context, "Download selesai", Toast.LENGTH_SHORT).show();
+            }
+        };
+        IntentFilter downloadIntentFilter = new IntentFilter(ACTION_DOWNLOAD_STATUS);
+        registerReceiver(downloadReceiver, downloadIntentFilter);
     }
 
     final int SMS_PERMISSION_CODE = 101;
@@ -29,6 +48,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v.getId() == R.id.btn_permission){
            PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_PERMISSION_CODE);
+        }else if (v.getId() == R.id.btn_download){
+            Intent downloadServiceIntent = new Intent(MainActivity.this, DownloadService.class);
+            startService(downloadServiceIntent);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (downloadReceiver != null){
+            unregisterReceiver(downloadReceiver);
         }
     }
 
